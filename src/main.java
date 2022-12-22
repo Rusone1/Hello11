b7ae90f7b07b50123a2b0b3559e013351f0ecfc0
Rusone <amegar@yandex.ru>
2022-12-22 14:23:08 +0300

    1s

---
+++ src/main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class main {
+
+public static void main(String[]args) {
+System.out.println("Переменные и типы данных");
+int age;
+age = 55;
+int god;
+god = 15;
+age = god + age;
+    System.out.println("Вывод: " + age + ".");
+
+
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Vvedite name");
+   String penis = scan.nextLine();
+System.out.println("Vawe name\n"  + penis);
+
+    }
+
+
+
+
+}
